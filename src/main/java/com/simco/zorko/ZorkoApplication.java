5e2f7c69d74a98a19d5ce15a626c155e9cdc459c
RD <rory.c.dickinson@gmail.com>
2019-12-12 00:55:06 -0600

    Impl simple SpringBoot and MavenWrapper
    Initial commit as Spring Boot project with Maven wrapper

---
+++ src/main/java/com/simco/zorko/ZorkoApplication.java
@@ -0,0 +1,29 @@
+package com.simco.zorko;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZorkoApplication
+implements CommandLineRunner {
+
+	private static Logger log = LoggerFactory.getLogger(ZorkoApplication.class);
+	
+	public static void main(String[] args) {
+		log.info("Starting the application");
+		SpringApplication.run(ZorkoApplication.class, args);
+		log.info("Application finished");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Executing: command line runner");
+		for (int i = 0; i < args.length; ++i) {
+			log.info("args[{}]: {}", i, args[i]);
+		}
+	}
+
+}
---
+++ src/test/java/com/simco/zorko/ZorkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simco.zorko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZorkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
